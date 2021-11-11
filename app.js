af9b31a0552687544234ab7a0d3a4510bdc32820
Krushnaraj004 <Krushnaraj11@gmail.com>
2021-11-11 09:30:53 +0530

    code updated 2

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use(express.json());
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.get("/",(req,res)=>{
+    res.send("you are good to go buddy");
+});
 
 app.use((req, res, next) => {
     console.log("query", req.query);
